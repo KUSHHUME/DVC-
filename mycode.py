bc4c2f4f3fe092da5508b04b526d2f588ffdc56b
KUSHHUME <kushsharma7378@gmail.com>
2026-02-04 11:55:29 +0530

    3rd version

--- mycode.py
+++ mycode.py
@@ -10,8 +10,10 @@ df = pd.DataFrame(data)
 
 new_row = {'Name': 'Ravi', 'Age': 28, 'City': 'San Francisco'}
 df.loc[len(df.index)] = new_row
-new_row = {'Name': 'Anshul', 'Age': 28, 'City': 'Alwar'}
-df.loc[len(df.index)] = new_row
+
+new_row2 = {'Name': 'Anshul', 'Age': 28, 'City': 'San Francisco'}
+df.loc[len(df.index)] = new_row2
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
